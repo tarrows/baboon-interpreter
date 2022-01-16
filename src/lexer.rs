568341b87be85a50ac7e6ccb4b19e3412150c068
tarrows <c10t@fa.ke>
2022-01-16 23:30:38 +0900

    lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -34,17 +34,69 @@ impl<'a> Lexer<'a> {
   }
 
   pub fn next_token(&mut self) -> Token {
+    self.skip_whitespaces();
+
     let token = match self.curr {
       '=' => Token::ASSIGN,
       ';' => Token::SEMICOLON,
+      '(' => Token::LPAREN,
+      ')' => Token::RPAREN,
+      ',' => Token::COMMA,
+      '+' => Token::PLUS,
+      '{' => Token::LBRACE,
+      '}' => Token::RBRACE,
       '\u{0}' => Token::EOF,
-      _ => Token::ILLEGAL,
+      c => {
+        if is_letter(c) {
+          // skip reading next char
+          return self.read_identifier();
+        } else if c.is_ascii_digit() {
+          // skip reading next char
+          return self.read_number();
+        } else {
+          Token::ILLEGAL
+        }
+      },
     };
 
     self.read_char();
 
     token
   }
+
+  fn skip_whitespaces(&mut self) {
+    while self.curr == ' ' || self.curr == '\t' || self.curr == '\n' || self.curr == '\r' {
+      self.read_char();
+    }
+  }
+
+  fn read_identifier(&mut self) -> Token {
+    let mut ident = String::new();
+
+    while is_letter(self.curr) {
+      ident.push(self.read_char());
+    }
+
+    if let Some(token) = Token::keyword(&ident) {
+      token
+    } else {
+      Token::IDENT(ident)
+    }
+  }
+
+  fn read_number(&mut self) -> Token {
+    let mut number = String::new();
+
+    while self.curr.is_ascii_digit() {
+      number.push(self.read_char());
+    }
+
+    Token::INT(number.parse().unwrap())
+  }
+}
+
+fn is_letter(c: char) -> bool {
+  ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_'
 }
 
 #[cfg(test)]
@@ -53,12 +105,22 @@ mod tests {
   use crate::token::Token;
 
   #[test]
-  fn generate_tokens() {
-    let input = "=3;";
+  fn test_generate_tokens() {
+    let input = r#"
+    let five = 5;
+    let ten = 10;
+    "#;
 
     let expected = [
+      Token::LET,
+      Token::IDENT("five".to_string()),
       Token::ASSIGN,
-      Token::ILLEGAL,
+      Token::INT(5),
+      Token::SEMICOLON,
+      Token::LET,
+      Token::IDENT("ten".to_string()),
+      Token::ASSIGN,
+      Token::INT(10),
       Token::SEMICOLON,
     ];
 
